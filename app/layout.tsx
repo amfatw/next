040580236db7b9efc15d37164966fd36583dba5b
Alexander <amfatw@mail.ru>
2025-01-26 11:28:33 +0700

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts';
+
+interface Props {
+  children: React.ReactNode
+}
+
+export default function RootLayout({children}: Props) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
